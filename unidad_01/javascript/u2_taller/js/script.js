f6e3669f8f95a3449f3d737949f290b03c0d67ef
Sulay Valeria Vergara Chávez <svergarac@est.ups.edu.ec>
2024-07-04 17:32:13 -0500

    tallerJS

---
+++ unidad_01/javascript/u2_taller/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('cargar').addEventListener('click', function() {
+    document.getElementById('imagen').style.display = 'block';
+});
+
+document.getElementById('eliminar').addEventListener('click', function() {
+    document.getElementById('imagen').style.display = 'none';
+});
