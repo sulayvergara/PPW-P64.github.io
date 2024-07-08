eb77888733e0ceeec028e7992e4523fa6311cd0e
Sulay Valeria Vergara Chávez <svergarac@est.ups.edu.ec>
2024-07-07 20:57:53 -0500

    u2taller

--- unidad_01/javascript/u2_taller/js/script.js
+++ unidad_01/javascript/u2_taller/js/script.js
@@ -1,7 +1,9 @@
 document.getElementById('cargar').addEventListener('click', function() {
     document.getElementById('imagen').style.display = 'block';
+    document.getElementById('texto-imagen').style.display = 'none';
 });
 
 document.getElementById('eliminar').addEventListener('click', function() {
     document.getElementById('imagen').style.display = 'none';
+    document.getElementById('texto-imagen').style.display = 'block';
 });
