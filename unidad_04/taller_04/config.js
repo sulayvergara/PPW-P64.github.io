4af0fc69ccdc6741c39f4b885cddf509d3bbcaf9
Sulay Valeria Vergara Chávez <svergarac@est.ups.edu.ec>
2024-08-13 21:00:32 -0500

    taller4

---
+++ unidad_04/taller_04/config.js
@@ -0,0 +1,7 @@
+const config = {
+    PORT: 3000,
+    //DB_URL: 'mongodb://dba:dba@localhost:27020/miapp?authSource=admin'
+    DB_URL: 'mongodb+srv://sulayvergara:<STUV18ss>@cluster0.0pmo6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+}
+
+module.exports = config
